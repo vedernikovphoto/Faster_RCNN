de760fac99ff65699ca758b8e1f0338e72be3f8b
vedernikovphoto <vedernikovphoto@gmail.com>
2023-09-15 18:49:23 +0300

    fixing the code

--- train.py
+++ train.py
@@ -26,25 +26,21 @@ def train_model(num_epochs=2, batch_size=2, lr=0.005, momentum=0.9, weight_decay
         list: A list of loss values recorded during training.
     """
 
-    # Use custom dataset and transformations
     dataset = MyDataset(root='D:\\Object detection\\CrowdHuman_train01\\Images',
                         annotations_file='D:\\Object detection\\annotation_train.odgt',
                         transforms=MyTransforms(train=True))
 
-    # Define DataLoader for training data
     data_loader_train = DataLoader(dataset,
                                    batch_size=batch_size,
                                    shuffle=True,
                                    num_workers=0,
                                    collate_fn=lambda x: tuple(zip(*x)))
 
-    # Define the device to use for computation
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     # Load a pre-trained model
     model = fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.COCO_V1)
 
-    # Define the number of classes
     num_classes = 2  # 1 class (person) + background
 
     # Get the number of input features for the classifier
@@ -53,26 +49,23 @@ def train_model(num_epochs=2, batch_size=2, lr=0.005, momentum=0.9, weight_decay
     # Replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-    # Move model to the right device
     model.to(device)
 
     # Construct an optimizer
     params = [p for p in model.parameters() if p.requires_grad]
     optimizer = optim.SGD(params, lr=lr, momentum=momentum, weight_decay=weight_decay)
 
-    # Define a learning rate scheduler
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
 
-    loss_values = []  # List to store loss values over epochs
+    loss_values = []
 
-    # Train the model for num_epochs epochs
+    # Train the model
     for epoch in range(num_epochs):
         epoch_loss = train_one_epoch(model, optimizer, data_loader_train, device, epoch, print_freq=4)
-        loss_values.append(epoch_loss)  # Append the loss value to the list
+        loss_values.append(epoch_loss)
         lr_scheduler.step()  # Update the learning rate
         torch.cuda.empty_cache()  # Clean up any unneeded CUDA memory
 
-    # Save the trained model weights
     torch.save(model.state_dict(), 'model_weights_final.pth')
 
     return loss_values
--- train_one_epoch.py
+++ train_one_epoch.py
@@ -1,25 +1,37 @@
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
-    model.train()  # Set the model to training mode
-    losses = []  # List to store loss values
+    """
+    Trains the model for one epoch and returns the average loss.
 
-    # Iterate over batches of data in the data loader
+    Args:
+        model: The model to be trained.
+        optimizer: The optimizer for training the model.
+        data_loader: DataLoader providing the training data.
+        device: The device on which to perform the computation (CPU or GPU).
+        epoch: The current epoch number.
+        print_freq: Frequency at which to print the loss values.
+
+    Returns:
+        float: The average loss over the epoch.
+    """
+
+    model.train()
+    losses = []
+
     for step, (images, targets) in enumerate(data_loader):
         images = list(image.to(device) for image in images)  # Move images to the specified device
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]  # Move targets to the specified device
 
-        loss_dict = model(images, targets)  # Compute the loss
+        loss_dict = model(images, targets)
 
         losses_dict = {k: v.item() for k, v in loss_dict.items()}  # Convert the loss tensor to Python float
-        loss_value = sum(losses_dict.values())  # Sum the losses
-        losses.append(loss_value)  # Append the sum of losses to the losses list
+        loss_value = sum(losses_dict.values())
+        losses.append(loss_value)
 
-        # Print the loss value every print_freq steps
         if step % print_freq == 0:
             print(f"Epoch: {epoch}, Iteration: {step}, Loss: {loss_value}")
 
-        optimizer.zero_grad()  # Reset the gradients
-        sum(loss_dict.values()).backward()  # Compute the gradient
-        optimizer.step()  # Update the model parameters
+        optimizer.zero_grad()
+        sum(loss_dict.values()).backward()
+        optimizer.step()
 
-    # Return the average loss over the epoch
     return sum(losses) / len(losses)
