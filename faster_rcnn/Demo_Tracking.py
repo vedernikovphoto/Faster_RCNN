584a8b617b6082d6a63c0516568786c101e03bf1
vedernikovphoto <vedernikovphoto@gmail.com>
2023-10-06 18:45:55 +0300

    that's just a simple commit!

--- faster_rcnn/Demo_Tracking.py
+++ faster_rcnn/Demo_Tracking.py
@@ -1,100 +1,71 @@
 from sort import *
-import random
+import torch
 import cv2
-
-def load_class_names(namesfile):
-    class_names = []
-    with open(namesfile, 'r') as fp:
-        lines = fp.readlines()
-    for line in lines:
-        line = line.rstrip()
-        class_names.append(line)
-    return class_names
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.transforms import functional as F
+from sort import Sort
 
 
-def detect_cv2_camera(cfgfile, weightfile):
-    #Заграузка сети
-    net = cv2.dnn.readNetFromDarknet(cfgfile, weightfile)
-    ln = net.getLayerNames()
-    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-    #Инициализируем трекер
+def detect_cv2_camera(model_path='model_weights_final.pth', confidence_threshold=0.8, video_path='/Users/aleksandrvedernikov/Desktop/people.mp4'):
+    # Initialize tracker
     mot_tracker = Sort()
-    #Используем YOLO
-    namesfile = 'coco.names'
-    class_names = load_class_names(namesfile)
-    #Камера
-    cap = cv2.VideoCapture(0)
-    #Генерим коробочкам разные цвета
-    color_list = []
-    for j in range(1000):
-        color_list.append(((int)(random.randrange(255)),(int)(random.randrange(255)),(int)(random.randrange(255))))
 
-    #go!
-    ret = True
-    while ret:
-        ret, img = cap.read()
-        if ret:
-            #Запустим сеть по картинке
-            blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416),
-                                         swapRB=True, crop=False)
+    # Load the Faster R-CNN model with custom weights
+    model = fasterrcnn_resnet50_fpn(pretrained=False, num_classes=3)
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
 
-            h,w,_ = img.shape
-            net.setInput(blob)
-            layerOutputs = net.forward(ln)
-            #Разберём все выходы
-            boxes=[]
-            confidences=[]
-            classIDs=[]
-            for output in layerOutputs:
-                for detection in output:
-                    scores = detection[5:]
-                    classID = np.argmax(scores)
-                    confidence = scores[classID]
-                    if confidence > 0.5:
-                        box = detection[0:4] * np.array([w, h, w, h])
-                        (centerX, centerY, width, height) = box.astype("int")
-                        x = int(centerX - (width / 2))
-                        y = int(centerY - (height / 2))
-                        boxes.append([x, y, int(width), int(height)])
-                        confidences.append(float(confidence))
-                        classIDs.append(classID)
+    # Use CUDA if available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
 
-            idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5,
-                                    0.3)
+    # Capture video from file or webcam
+    cap = cv2.VideoCapture(video_path)
 
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Failed to grab frame or video ended")
+                break
 
-            result_img = np.copy(img)
-            dets = []
-            count_detection=0
-            for j in range(len(idxs)):
-                name = class_names[classIDs[idxs[j][0]]]
-                if name == 'person':
-                    count_detection+=1
-            if count_detection>0:
-                detects = np.zeros((count_detection,5))
-                count=0
-                #Подготовим в формат который ест трекер
-                for j in range(len(idxs)):
-                    b = boxes[j]
-                    name = class_names[classIDs[idxs[j][0]]]
-                    if name == 'person':
-                        x1 = int(b[0])
-                        y1 = int(b[1])
-                        x2 = int((b[0] + b[2]))
-                        y2 = int((b[1] + b[3]))
-                        box = np.array([x1,y1,x2,y2,confidences[idxs[j][0]]])
-                        detects[count,:] = box[:]
-                        count+=1
-                #Подаём в трекер и сразу имеем результат!
-                if len(detects)!=0:
-                    trackers = mot_tracker.update(detects)
-                    for d in trackers:
-                        result_img = cv2.rectangle(result_img, ((int)(d[0]), (int)(d[1])), ((int)(d[2]), (int)(d[3])), color_list[(int)(d[4])], 2)
-            cv2.imshow('demo', result_img)
-            cv2.waitKey(1)
-    cap.release()
+            rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            tensor_image = F.to_tensor(rgb_image).unsqueeze(0).to(device)
 
+            with torch.no_grad():
+                prediction = model(tensor_image)
+
+            boxes = prediction[0]['boxes'].cpu().numpy()
+            scores = prediction[0]['scores'].cpu().numpy()
+            selected_indices = scores >= confidence_threshold
+            selected_boxes = boxes[selected_indices]
+
+            # Continue if no boxes are detected
+            if len(selected_boxes) == 0:
+                cv2.imshow('frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                continue
+
+            trackers = mot_tracker.update(selected_boxes)
+
+            for track in trackers:
+                bbox = track[:4]
+                track_id = int(track[4])
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 0, 0), 2)
+                cv2.putText(frame, str(track_id), (int(bbox[0]), int(bbox[1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                            (0, 255, 0), 2)
+
+            cv2.imshow('frame', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    detect_cv2_camera('yolov3-tiny.cfg', 'yolov3-tiny.weights')
+    detect_cv2_camera()
